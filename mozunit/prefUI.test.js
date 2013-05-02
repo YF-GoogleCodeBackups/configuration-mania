43b7145b438aeda5b1cfab8aaf4fedb3fcd8b1b9
cat_in_136 <cat_in_136@localhost>
2013-05-02 12:18:57 +0900

    fix miscellaneous stuff so as to pass the mozunit test.

--- mozunit/prefUI.test.js
+++ mozunit/prefUI.test.js
@@ -136,7 +136,7 @@ tc.tests = {
   },
   testOnLocaleMatchOSSyncFrom: function() {
     let checkbox = this.document.querySelector("checkbox[preference='intl.locale.matchOS']");
-    let target = this.document.querySelector("checkbox[preference='general.useragent.locale']");
+    let target = this.document.querySelector("textbox[preference='general.useragent.locale']");
 
     for (let i = 0; i < 2; i++) {
       assert.equals(checkbox.checked, target.disabled);
