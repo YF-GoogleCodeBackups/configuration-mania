4b5e05595d5d747e43cfb24178239fbe937e7ee2
cat_in_136 <cat_in_136@localhost>
2015-01-29 00:43:40 +0900

    Let "UI"->"Other"->"Apply instance and the dialog is modeless" disabled unless "UI"->"Other"->"Open Preference in tab instead of dialog" is checked. (issue #163 )

--- mozunit/prefUI.test.js
+++ mozunit/prefUI.test.js
@@ -144,4 +144,15 @@ tc.tests = {
       checkbox.click();
     }
   },
+  testOnPreferenceInContentSyncFrom: function() {
+    let checkbox = this.document.querySelector("checkbox[preference='browser.preferences.inContent']");
+    let target1 = this.document.querySelector("checkbox[preference='browser.preferences.instantApply']");
+    let target2 = this.document.querySelector("checkbox[preference='browser.preferences.instantApply']+description");
+
+    for (let i = 0; i < 2; i++) {
+      assert.equals(checkbox.checked, target1.disabled);
+      assert.equals(checkbox.checked, target2.disabled);
+      checkbox.click();
+    }
+  }
 }
