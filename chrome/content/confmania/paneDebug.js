c5834f9d85d24c901f7b9888586c8f15a9894d94
cat_in_136 <cat_in_136@localhost>
2015-08-30 18:39:08 +0900

    fix issue #195 "Debug"->"For Debbuging"->"Enable JavaScript dump() output"->"Output file"->"Browse..." opens a "Open" file dialog, not "Save" file dialog.

--- chrome/content/confmania/paneDebug.js
+++ chrome/content/confmania/paneDebug.js
@@ -2,47 +2,46 @@
 gPrefWindow.prefDebug = {
   init : function() {
   },
-  _openBrowse: function(aFilters, aTargetID, aValueType) {
-    // Open File Dialog
+  _openBrowse: function(aFilters, aTargetID, aValueTypes) {
+    let targetElem = document.getElementById(aTargetID);
+    let valueTypes = (aValueTypes)? aValueTypes.split(/\s+/) : [];
+    let isUri = valueTypes.some(function(v) { return (v === "uri") || (v === "url"); });
+    let isDirectory = valueTypes.some(function(v) { return (v === "directory"); });
+    let isSave = valueTypes.some(function(v) { return (v === "save"); });
+
     const nsIFilePicker = Components.interfaces.nsIFilePicker;
-    var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-    if (aValueType == "url" || aValueType == "uri") {
-      fp.init(window, document.title, nsIFilePicker.modeOpen);
-    } else if (aValueType == "directory") {
+    let fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+    if (isDirectory) {
       fp.init(window, document.title, nsIFilePicker.modeGetFolder);
+    } else if (isSave) {
+      fp.init(window, document.title, nsIFilePicker.modeSave);
     } else {
       fp.init(window, document.title, nsIFilePicker.modeOpen);
     }
 
-    Array.forEach(aFilters, function(filter) {
-      if (typeof(filter) == "number") {
+    for (let filter of aFilters) {
+      if (typeof(filter) === "number") {
         fp.appendFilters(filter);
       } else {
         fp.appendFilter(filter.name, filter.filter);
       }
-    });
+    }
 
-    try{
-      var targetElem = document.getElementById(aTargetID);
-      var file = null;
-      if (aValueType == "url" || aValueType == "uri") {
-        file = new FileUtils.File(OS.Path.fromFileURI((targetElem.value == "")? targetElem.placeholder : targetElem.value));
+    try {
+      let file = null;
+      if (isUri) {
+        file = new FileUtils.File(OS.Path.fromFileURI((targetElem.value === "")? targetElem.placeholder : targetElem.value));
       } else {
-        file = new FileUtils.File((targetElem.value == "")? targetElem.placeholder : targetElem.value);
+        file = new FileUtils.File((targetElem.value === "")? targetElem.placeholder : targetElem.value);
       }
 
       fp.displayDirectory = file.parent;
       fp.defaultString = file.leafName;
-    }catch(ex){}
+    } catch (ex) {}
 
-    if(fp.show() == nsIFilePicker.returnOK) {
-      if (aValueType == "url" || aValueType == "uri") {
-        targetElem.value = fp.fileURL.spec;
-      } else if (aValueType == "directory") {
-        targetElem.value = fp.file.path;
-      } else {
-        targetElem.value = fp.file.path;
-      }
+    let ret = fp.show();
+    if ((ret === nsIFilePicker.returnOK) || (ret === nsIFilePicker.returnReplace)) {
+      targetElem.value = (isUri)? fp.fileURL.spec : fp.file.path;
       gPrefWindow.getCurrentPrefPane().userChangedValue(targetElem);
     }
   },
@@ -53,7 +52,7 @@ gPrefWindow.prefDebug = {
   },
   onDebugDumpFileBrowse : function () {
     const filters = [Components.interfaces.nsIFilePicker.filterAll];
-    this._openBrowse(filters, "debug-dump-file", "file");
+    gPrefWindow.prefDebug._openBrowse(filters, "debug-dump-file", "file save");
   },
   onDebugDumpFileReset : function () {
     gPrefWindow.resetPref(document.getElementById("browser.dom.window.dump.file"));
